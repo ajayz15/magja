a753994dc87f13287d046e66c59de5bc1f3d613c
andre@rockstore.com.br <andre@rockstore.com.br@b53afa6a-2257-11df-8f40-0d449d17a094>
2011-11-02 15:10:00 +0000

    [maven-release-plugin]  copy for tag magja-0.0.4

---
+++ tags/magja-0.0.4/src/main/java/com/google/code/magja/model/order/Invoice.java
@@ -0,0 +1,22 @@
+/**
+ * @author andre
+ *
+ */
+package com.google.code.magja.model.order;
+
+import com.google.code.magja.model.BaseMagentoModel;
+
+public class Invoice extends BaseMagentoModel {
+
+	private static final long serialVersionUID=1812284345946352134L;
+
+	/* (non-Javadoc)
+	 * @see com.google.code.magja.model.BaseMagentoModel#serializeToApi()
+	 */
+	@Override
+	public Object serializeToApi() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ tags/magja-0.0.4/src/main/java/com/google/code/magja/service/order/InvoiceRemoteService.java
@@ -0,0 +1,12 @@
+/**
+ * @author andre
+ *
+ */
+package com.google.code.magja.service.order;
+
+import com.google.code.magja.model.order.Invoice;
+import com.google.code.magja.service.GeneralService;
+
+public interface InvoiceRemoteService extends GeneralService<Invoice> {
+
+}
